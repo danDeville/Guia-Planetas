3afb76f0c26013974e025cdad24128d5af9978aa
dandeville <dandeville26@gmail.com>
2022-03-29 10:38:25 -0500

    Modificaciones a Planetas.js

--- scripts/Planetas.js
+++ scripts/Planetas.js
@@ -1,6 +1,6 @@
-let nombrePlanetas = Array ('Tierra', 'Marte')
+let nombrePlanetas = Array ('Tierra', 'Marte', 'Jupiter')
 let distancia = [6,7, '67']
-const tamaño = []
+const tamaño = [10, 20, 30]
 
 // let i = 0
 // while (i <= nombrePlanetas.length -1) {
@@ -13,10 +13,12 @@ const tamaño = []
 //   console.log(nombrePlanetas[index])
 // }
 
+// for Off
 for (const planeta of nombrePlanetas) {
   console.log(planeta)
 }
 
+// for On
 for (const key in distancia) {
   if (distancia[key] === '5') {
     console.log('Se encontró la distancia ')
@@ -24,3 +26,21 @@ for (const key in distancia) {
     console.log('No se encontró la distancia')
   }
 }
+
+// forEach
+nombrePlanetas.forEach((value, index) => {
+  console.log('planeta', index, value)
+})
+
+// Map
+nombrePlanetas.map((planetas, index) => {
+  return (
+    console.log(`El planeta ${planetas} tiene un tamaño de ${tamaño[index]}`)
+  )
+})
+
+let resultadoMap = nombrePlanetas.map((planetas, index) => {
+  return (
+    console.log(`El planeta ${planetas} tiene un tamaño de ${tamaño[index]}`)
+  )
+})
